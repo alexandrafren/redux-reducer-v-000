9b96d2f1924c2ed4e42e9dc7f3515d98370dc02c
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-12 19:13:28 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,3 +1,8 @@
 export function manageFriends(state, action){
-
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
